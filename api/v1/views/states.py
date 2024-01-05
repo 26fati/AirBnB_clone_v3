aab3cfcd4ecd4b4fd783aa5a32aaa8a2a3186066
26fati <fatibeautiful1@gmail.com>
2024-01-05 23:10:52 +0100

    documentation

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" objects that handle all default RestFul API actions for States """
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models.state import State
