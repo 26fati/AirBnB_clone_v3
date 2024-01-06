4ce82cad28ddc16823bd71f378f5013ee5708b20
26fati <fatibeautiful1@gmail.com>
2024-01-06 12:48:20 +0100

    task 9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -49,8 +49,6 @@ def create_user():
     """
     if not request.get_json():
         abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
     if 'email' not in request.get_json():
         abort(400, description="Missing email")
     if 'password' not in request.get_json():
